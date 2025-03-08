024a8c258e825190a4d1e47bf498a70389c57a8e
Pratyush Raj Giri <asiandadx@gmail.com>
2025-03-08 09:24:10 +0545

    Added a new remainder feature

--- Projects/lp.cpp
+++ Projects/lp.cpp
@@ -25,6 +25,9 @@ int multiplication(int a,int b){
 double multiplication(double a, double b){
     return a*b;
     }
+int mod(int a,int b){
+    return a%b;
+}
 int verification(){
     int i;
     cout <<"Are u using integer or double\n"<< "1 if integer 2 if double";
@@ -54,6 +57,8 @@ int main(){
             break;
             case '/':cout <<division(a,b);
             break;
+            case '%':cout <<mod(a,b);
+            break;
             default:{
             cout<<"Invalid operation\n";
             goto out;}
@@ -76,6 +81,9 @@ int main(){
                 break;
                 case '/':cout <<division(c,d);
                 break;
+                case '%':cout <<"Mod is not a feature for double\n";
+                goto again;
+                break;
                 default:{
                 cout<<"Invalid operation\n";
                 goto fout;}
@@ -85,5 +93,6 @@ int main(){
                 cout<<"write 0 or 1\n";
                 goto again;
             }  
+        return 0;
 }
 
